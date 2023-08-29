10a9fe1d76fe689828652b943fcd45a3e2b5b7d6
seanas <anasse.el-bourachdi@epitech.eu>
2023-08-29 11:08:59 +0200

    reformat code

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -1,28 +1,30 @@
 import React from "react";
 import Nav from "../components/layout/Nav";
-import Breadcrumbs from "@mui/material/Breadcrumbs";
-import { Typography } from "@mui/material";
-import { BiTime } from "react-icons/bi";
+import BreadcrumbsComponent from "@mui/material/Breadcrumbs";
+import {Typography} from "@mui/material";
+import {BiTime} from "react-icons/bi";
+
+function CustomBreadcrumbs() {
+    return (
+        <BreadcrumbsComponent aria-label="breadcrumb" separator="/">
+            <BiTime size={20}/>
+            <Typography
+                key="3"
+                color="#666666"
+                style={{
+                    fontWeight: 300,
+                }}
+            >
+                Réservations
+            </Typography>
+        </BreadcrumbsComponent>
+    );
+}
 
 export default function Reservation() {
-  return (
-    <Nav
-      Breadcrumbs={() => (
-        <Breadcrumbs aria-label="breadcrumb" separator="/">
-          <BiTime size={20} />
-          <Typography
-            key="3"
-            color="#666666"
-            style={{
-              fontWeight: 300,
-            }}
-          >
-            Réservations
-          </Typography>
-        </Breadcrumbs>
-      )}
-    >
-      hello
-    </Nav>
-  );
+    return (
+        <Nav Breadcrumbs={CustomBreadcrumbs}>
+            hello
+        </Nav>
+    );
 }
